b68a2375e3cf54465cb061c28e8caf6c71a51117
Tundeakinbami-dev <abidemibabatunde332@gmail.com>
2026-09-24 16:06:08 +0100

    made a slight modification to the index.html

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const greeting = "Hello World";
 console.log(greeting);
 
 const selections = ["rock", "paper", "scissors"];
-const userInput = prompt("What do you want to choose: ").toLowerCase();
+const userInput = prompt("Choose either rock, paper or scissors: ").toLowerCase();
 
 function rockPaperscissors() {
   if (!selections.includes(userInput)) {
@@ -17,22 +17,22 @@ function rockPaperscissors() {
   console.log(`\ncomputerChoice: ${computerChoice}`);
 
   if (computerChoice === userInput) {
-      console.log("Its a tie");
-      alert("its a tie");
+    console.log("Its a tie");
+    alert("its a tie");
     return;
-  } else if (userInput == "rock" && computerChoice == "sciccors") {
-      console.log("User wins");
-      alert("user wins");
+  } else if (userInput == "rock" && computerChoice == "scissors") {
+    console.log("User wins");
+    alert("user wins");
   } else if (userInput == "paper" && computerChoice == "rock") {
-      console.log("User wins");
-      alert("user wins");
+    console.log("User wins");
+    alert("user wins");
   } else if (userInput == "scissors" && computerChoice == "paper") {
-      console.log("user wins");
-      alert("user wins");
+    console.log("user wins");
+    alert("user wins");
   } else {
-      console.log("Computer Wins!");
-      alert("Computer wins");
+    console.log("Computer Wins!");
+    alert("Computer wins");
   }
 }
 
-rockPaperscissors()
+rockPaperscissors();
