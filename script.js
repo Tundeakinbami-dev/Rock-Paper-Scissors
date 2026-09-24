236eddd7249f71b8265c44f10036eb8a4ab52284
Tundeakinbami-dev <abidemibabatunde332@gmail.com>
2026-09-24 15:29:55 +0100

    created a code for the rock paper scissors game

---
+++ script.js
@@ -0,0 +1,38 @@
+const greeting = "Hello World";
+console.log(greeting);
+
+const selections = ["rock", "paper", "scissors"];
+const userInput = prompt("What do you want to choose: ").toLowerCase();
+
+function rockPaperscissors() {
+  if (!selections.includes(userInput)) {
+    console.log("invalid selection");
+    return;
+  }
+
+  const computer = Math.floor(Math.random() * selections.length);
+  const computerChoice = selections[computer];
+
+  console.log(`\nYou Choose: ${userInput}`);
+  console.log(`\ncomputerChoice: ${computerChoice}`);
+
+  if (computerChoice === userInput) {
+      console.log("Its a tie");
+      alert("its a tie");
+    return;
+  } else if (userInput == "rock" && computerChoice == "sciccors") {
+      console.log("User wins");
+      alert("user wins");
+  } else if (userInput == "paper" && computerChoice == "rock") {
+      console.log("User wins");
+      alert("user wins");
+  } else if (userInput == "scissors" && computerChoice == "paper") {
+      console.log("user wins");
+      alert("user wins");
+  } else {
+      console.log("Computer Wins!");
+      alert("Computer wins");
+  }
+}
+
+rockPaperscissors()
